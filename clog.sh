44595730f27e08cb877667242d77112133caeee5
Realz Code <114070103+realzcode@users.noreply.github.com>
2023-07-16 03:13:33 +0700

    clog

--- clog.sh
+++ clog.sh
@@ -15,36 +15,31 @@ read -rp "Rock n Roll ? (Y/N): " choice
 
 if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
 
-	echo "Starting log clearing process..."
+	echo "\nStarting log clearing process..."
 	echo "--------------------------------"
-	
+
 	find "/var/log" -type f -name "*log*" | while read -r clog; do
 	    rm -rf $clog
 	    echo "Clearing... $clog [done]"
 	done
-	
+
 	cat /dev/null > /var/log/utmp 
 	echo "Clearing... /var/log/utmp [done]"
-	
+
 	cat /dev/null > /var/log/wtmp
 	echo "Clearing... /var/log/wtmp [done]"
-	
+
 	cat /dev/null > /var/log/btmp
 	echo "Clearing... /var/log/btmp [done]"
-	
+
 	rm -rf /var/log/journal/*
 	echo "Clearing... journalctl [done]"
-	
-	cat /dev/null > "$HOME/.bash_history"
-	bash -c "history -c"
-	echo "Clearing... bash_history [done]"
-	
-	cat /dev/null > "$HOME/.zsh_history"
-	bash -c "history -p"
-	echo "Clearing... zsh_history [done]"
-	
+
 	echo "-------------------------------"
 	echo "Log clearing process completed."
+
+	echo "\n* Some commands are done manually *"
+	echo "Clear History Command: history -c"
 
 else
     echo "Bye :p"
